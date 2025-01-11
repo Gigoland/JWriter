412e6b32364aea6a90ec2309cb3e90984a74ffd2
GigolDev <gigolnft@gmail.com>
2025-01-11 22:16:40 +0100

    Version beta

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# JWriter.js
-Javascript alphabet writer
+# JWriter.js beta
+Javascript Alphabet Writer
+
+Example:
+
+// Onload
+document.addEventListener('DOMContentLoaded', () => {
+  // Language letters convert
+  if (document.querySelector('[lang=ka]')) {
+    document.querySelectorAll('[lang=ka]').forEach(($element) => {
+      $element.addEventListener('keypress', (event) => {
+        return jWriter(
+            $element,
+            event,
+            jWriterAlphabetLibrary.ka.mkhedruli
+        );
+      });
+    });
+  }
+});
+
+@todo more doc ;)
---
+++ jwriter.js
@@ -0,0 +1,85 @@
+/*!
+ * JWriter.js v0.0.1 beta
+ * https://www.Gigoland.com
+ * Released under the MIT License
+ */
+const jWriterAlphabetLibrary = {
+  'ka': {
+    'asomtavruli': {
+      'alphabet': 'abgdevzTiklmnopJrstufqRySCcZwWxjh',
+      'unicode': 4256
+    },
+    'mkhedruli': {
+      'alphabet': 'abgdevzTiklmnopJrstufqRySCcZwWxjhHI',
+      'unicode': 4304
+    },
+    'mtavruli': {
+      'alphabet': 'abgdevzTiklmnopJrstufqRySCcZwWxjhHI',
+      'unicode': 7312
+    },
+    'nuskhuri': {
+      'alphabet': 'abgdevzTiklmnopJrstufqRySCcZwWxjh',
+      'unicode': 11520
+    },
+  }
+};
+
+const jWriter = ($e, e, alphabetLibrary) => {
+  e.stopPropagation();
+  String.prototype.jWriterConvert = function(params) {
+    let letter,
+        result = []
+    ;
+    for (let search = 0; search < this.length; search++) {
+        letter = this.substring(search, 1);
+        if ((position = params.alphabet.indexOf(letter)) >= 0) {
+          result.push(String.fromCharCode(position + params.unicode));
+        } else {
+          result.push(letter);
+        }
+    }
+    return result.join('');
+  };
+  Element.prototype.jWriterValue = function(val) {
+    this.focus();
+    if (document.selection) {
+      let range = document.selection.createRange();
+      if (range) {
+        range.text = val;
+      }
+    } else if (this.selectionStart !== undefined) {
+      with (this) {
+        var scrolltop = scrollTop,
+            start = selectionStart,
+            end = selectionEnd
+        ;
+      }
+      this.value = this.value.substring(0, start)
+        + val
+        + this.value.substring(end, this.value.length)
+      ;
+      this.scrollTop = scrolltop;
+      this.setSelectionRange(
+        start + val.length,
+        start + val.length
+      );
+    } else {
+      this.value += val;
+      this.setSelectionRange(
+        this.value.length,
+        this.value.length
+      );
+    }
+  };
+  if (/input|textarea/i.test($e.nodeName)) {
+    const charCode = String.fromCharCode(e.keyCode),
+          converted = charCode.jWriterConvert(
+            alphabetLibrary
+          )
+    ;
+    if (converted !== charCode) {
+      e.preventDefault();
+      return $e.jWriterValue(converted);
+    }
+  }
+};
